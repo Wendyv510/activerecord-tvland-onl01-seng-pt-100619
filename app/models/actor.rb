b708fba5df3eacdf56218ece10d59c7a424caa90
Wendy Harris <wendyv510@hotmail.com>
2020-01-11 16:49:31 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+    
+    has_many :characters 
+    has_many :shows, through: :characters 
+    
+  def full_name(first_name) 
+    "#{self.first_name}#{self.last_name}" 
+  # actor = Actor.new(:first_name => "first_name", :last_name => "last_name")
+   # actor.save
+  end 
+  
+  def list_roles 
+    self.characters.map {|c| "#{c.name} - #{c.show.name}"}
+  end 
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
+  has_many :characters 
+  has_many :actors, through: :characters 
+  
+  belongs_to :network
+  
+  def actors_list
+    #Show.actors.collect {|actor| actor.full_name}
+    self.actors.map {|c| "#{c.name} - #{c.show.name}"}
+  end 
+  
+  
+    
   
 end
